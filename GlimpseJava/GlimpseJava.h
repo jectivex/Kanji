4d6694d0dd46146698f39af73c3d076da6074e49
Marc Prud'hommeaux <mwp1@cornell.edu>
2015-03-06 23:32:33 -0500

    Initial Commit

---
+++ GlimpseJava/GlimpseJava.h
@@ -0,0 +1,19 @@
+//
+//  GlimpseJava.h
+//  GlimpseJava
+//
+//  Created by Marc Prud'hommeaux on 3/6/15.
+//  Copyright (c) 2015 io.glimpse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GlimpseJava.
+FOUNDATION_EXPORT double GlimpseJavaVersionNumber;
+
+//! Project version string for GlimpseJava.
+FOUNDATION_EXPORT const unsigned char GlimpseJavaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GlimpseJava/PublicHeader.h>
+
+
