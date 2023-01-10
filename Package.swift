ec7201eabf6a0f4b39c061e6e6941fcf3593cf33
marcprux <marcprux@users.noreply.github.com>
2023-01-10 22:51:22 +0000

    deploy: a7b1f423a434f4479a091278f32b49841e9f9aea

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ import PackageDescription
 #if os(macOS)
 let testLinkerSettings: [LinkerSetting] = [
     .unsafeFlags([
-        "-L/opt/homebrew/opt/openjdk/libexec/openjdk.jdk/Contents/Home/lib", // Homebrew macOS
+        "-L/opt/homebrew/opt/openjdk/libexec/openjdk.jdk/Contents/Home/lib", // Homebrew macOS ARM
+        "-L/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home/lib", // Homebrew macOS Intel
     ]),
     .linkedLibrary("jsig"),
     .linkedLibrary("jli"),
